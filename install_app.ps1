de05d348c5a4379e1543479af22a943653556db4
colefromportland <colefromportland@gmail.com>
2019-09-06 11:09:20 -0600

    comments / formatting

--- install_app.ps1
+++ install_app.ps1
@@ -1,6 +1,7 @@
-# reinstall all apps for all user
+# installing applications (often an issue related to no items on Start Menu)
 
+## reinstall all Windows applications
 Get-AppXPackage -AllUsers | Foreach {Add-AppxPackage -DisableDevelopmentMode -Register "$($_.InstallLocation)\AppXManifest.xml"}
 
-# install a specific package
+## install a specific package
 Get-AppXPackage -Register "C:\Program Files\WindowsApps\*Windows.Photos*\AppxManifest.xml" -DisableDevelopmentMode
